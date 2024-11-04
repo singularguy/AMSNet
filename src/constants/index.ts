ba938e95548a46137f5307e43f43a85aa03c59ab
shaochengyuan <97899030+singularguy@users.noreply.github.com>
2024-11-04 08:42:20 +0800

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,9 +1,10 @@
 /**
  * 本地后端地址
  */
-export const BACKEND_HOST_LOCAL = "http://amsnet.free.idcfengye.com/api";
+// export const BACKEND_HOST_LOCAL = "http://amsnet.free.idcfengye.com/api";
+export const BACKEND_HOST_LOCAL = "http://localhost:8101/api";
 
 /**
  * 线上后端地址
  */
-export const BACKEND_HOST_PROD = "http://amsnet.free.idcfengye.com";
+export const BACKEND_HOST_PROD = "http://localhost:8101/api";
